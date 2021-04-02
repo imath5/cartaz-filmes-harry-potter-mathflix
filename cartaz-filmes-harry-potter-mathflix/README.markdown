cd14c7c8aa21293cba852793be251129a5e3a4c1
Matheus de Souza Carvalho <73304785+imath5@users.noreply.github.com>
2021-04-02 19:01:23 -0300

    Add files via upload

---
+++ cartaz-filmes-harry-potter-mathflix/README.markdown
@@ -0,0 +1,5 @@
+# Cartaz filmes Harry Potter / Mathflix
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/imaths/pen/PoWZmeZ](https://codepen.io/imaths/pen/PoWZmeZ).
+
+
---
+++ cartaz-filmes-harry-potter-mathflix/dist/script.js
@@ -0,0 +1,6 @@
+var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BNjQ3NWNlNmQtMTE5ZS00MDdmLTlkZjUtZTBlM2UxMGFiMTU3XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMTcxODgwMDkxNV5BMl5BanBnXkFtZTYwMDk2MDg3._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMTI1NDMyMjExOF5BMl5BanBnXkFtZTcwOTc4MjQzMQ@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMTM0NTczMTUzOV5BMl5BanBnXkFtZTYwMzIxNTg3._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BNzU3NDg4NTAyNV5BMl5BanBnXkFtZTcwOTg2ODg1Mg@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMTQ2OTE1Mjk0N15BMl5BanBnXkFtZTcwODE3MDAwNA@@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+
+for (var i = 0; i < listaFilmes.length; i++){
+  
+  document.write("<img src=" +listaFilmes[i]+ ">")
+}
